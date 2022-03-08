76ccf53cb0781be0d3dac0eecf3f6e8cea6a880b
Sai Sashreek D <sai@5thdt.com>
2022-03-08 11:17:08 +0530

    Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkMzNA-4vSjGF4-23fRACRKxB7C9Xweuo',
+  authDomain: 'schooladminpanel-80f71.firebaseapp.com',
+  projectId: 'schooladminpanel-80f71',
+  storageBucket: 'schooladminpanel-80f71.appspot.com',
+  messagingSenderId: '43097420462',
+  appId: '1:43097420462:web:0b7ff3e30fee0bfb40f33f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 /*
  * For easier debugging in development mode, you can import the following file
